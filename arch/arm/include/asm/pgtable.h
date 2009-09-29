6b663e60b1c60b28d83bc775993fa91f6c9d5d74
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2009-09-29 10:14:56 +0200

    "Best-effort" FCSE: Handle shared mappings
    Account for writable shared mappings, so as to be able to flush cache
    when switching out a process with such a mapping.
    
    This accounting is done by adding the L_PTE_SHARED flag to page
    belonging to such mappings in make_coherent(), then, set_pte_at() is
    intercepted to account for both addition and removal of pages with
    this flag set.
    
    Note that using the L_PTE_SHARED flag makes the FCSE patch
    incompatible with ARMv6+ and Xscale3, but the FCSE feature is
    deprecated in ARMv6 anyway.
    
    Signed-off-by: Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -151,6 +151,8 @@ extern pgprot_t phys_mem_access_prot(struct file *file, unsigned long pfn,
 	struct mm_struct *_mm = (mm);				\
 	unsigned long _addr = (addr);				\
 	unsigned long _val = (val);				\
+	if (pte_present(_val) && ((_val) & L_PTE_SHARED))	\
+		--_mm->context.fcse.shared_dirty_pages;		\
 	if (pte_present(_val) && _addr < TASK_SIZE) {		\
 		if (_addr >= FCSE_TASK_SIZE			\
 		    && 0 == --_mm->context.fcse.high_pages)	\
@@ -162,6 +164,13 @@ extern pgprot_t phys_mem_access_prot(struct file *file, unsigned long pfn,
 	struct mm_struct *_mm = (mm);					\
 	unsigned long _addr = (addr);					\
 	unsigned long _val = (val);					\
+	if (pte_present(_val) && (_val & L_PTE_SHARED)) {		\
+		if ((_val & (PTE_CACHEABLE | L_PTE_RDONLY | L_PTE_DIRTY)) \
+		    != (PTE_CACHEABLE | L_PTE_DIRTY))			\
+			_val &= ~L_PTE_SHARED;                          \
+		else                                                    \
+			++_mm->context.fcse.shared_dirty_pages;         \
+	}                                                               \
 	if (pte_present(_val)						\
 	    && _addr < TASK_SIZE && _addr >= FCSE_TASK_SIZE) {		\
 		unsigned long pid = _addr / FCSE_TASK_SIZE;		\
