9c15d680c813c10875735d4c7691153e0beca83b
Philippe Gerum <rpm@xenomai.org>
2012-09-03 11:00:34 +0200

    x86/ipipe: ipipe_head_switch_mm -> ipipe_switch_mm_head

--- arch/x86/include/asm/mmu_context.h
+++ arch/x86/include/asm/mmu_context.h
@@ -83,7 +83,7 @@ static inline void switch_mm(struct mm_struct *prev, struct mm_struct *next,
 	hard_cond_local_irq_restore(flags);
 }
 
-#define ipipe_head_switch_mm(prev, next, tsk) \
+#define ipipe_switch_mm_head(prev, next, tsk) \
 	__switch_mm(prev, next, tsk)
 
 #define activate_mm(prev, next)			\
