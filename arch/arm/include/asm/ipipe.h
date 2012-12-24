3b3b1d3969106b561ec5fee6c0006eff2f1bc1bb
Philippe Gerum <rpm@xenomai.org>
2012-12-24 17:01:09 +0100

    arm/ipipe: API revision 2

--- arch/arm/include/asm/ipipe.h
+++ arch/arm/include/asm/ipipe.h
@@ -37,7 +37,7 @@ extern unsigned long arm_return_addr(int level);
 #include <linux/ipipe_trace.h>
 #include <mach/irqs.h>
 
-#define IPIPE_CORE_RELEASE	1
+#define IPIPE_CORE_RELEASE	2
 
 struct ipipe_domain;
 
