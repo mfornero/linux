1be8929cfad3f1ec4ca4064e3eaf7f0a2b599c56
Matthew Fornero <mfornero@aanddtech.com>
2013-01-28 13:07:38 -0500

    script: fixed the kern_make.sh script

--- kern_make.sh
+++ kern_make.sh
@@ -5,6 +5,12 @@ ROOTFS_STAGING=${ROOTFS_DIR}/_rootfs
 # Setup the .config file
 make ARCH=arm xilinx_zynq_adt_defconfig
 # Build and install the kernel and modules
-make -j4 INSTALL_MOD_PATH=${ROOTFS_STAGING} ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- zImage modules modules_install
+make -j4 ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- zImage modules 
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi INSTALL_MOD_PATH=${ROOTFS_STAGING} modules_install
+# Remove the symlinks
+for D in `ls ${ROOTFS_STAGING}/lib/modules/`; do
+	rm ${ROOTFS_STAGING}/lib/modules/${D}/source
+	rm ${ROOTFS_STAGING}/lib/modules/${D}/build
+done
 # Build the device tree
 ./scripts/dtc/dtc -I dts -O dtb -o ${ROOTFS_DIR}/misc/devicetree.dtb arch/arm/boot/dts/zynq-zed-adt.dts
