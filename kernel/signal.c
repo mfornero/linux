cb6f9529c88661f7beb614caf6441749fbd8435f
Philippe Gerum <rpm@xenomai.org>
2013-09-19 10:22:41 +0200

    ipipe: plug hole in reporting signal receipt to the head domain
    A task in PTRACED state receiving a signal may be controlled by the
    head domain, in which case we also have to report this event to that
    domain, so the required fixups to receive the signal can be performed
    there.

--- kernel/signal.c
+++ kernel/signal.c
@@ -937,8 +937,11 @@ static inline int wants_signal(int sig, struct task_struct *p)
 		return 0;
 	if (sig == SIGKILL)
 		return 1;
-	if (task_is_stopped_or_traced(p))
+	if (task_is_stopped_or_traced(p)) {
+		if (!signal_pending(p))
+			__ipipe_report_sigwake(p);
 		return 0;
+	}
 	return task_curr(p) || !signal_pending(p);
 }
 
